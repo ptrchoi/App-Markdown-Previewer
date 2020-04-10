2bf9d61c6bd46697ae3ee6c9e953b15ab6d1cec6
Peter Choi <ptrchoi@gmail.com>
2020-04-10 01:38:16 -0500

    Updated Firebase config to align with consolidated backend Firebase projects

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,13 +2,14 @@ import firebase from 'firebase';
 
 // App's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyCugWjRiIlkR4ddOhII0D1in3l3DkWGJhk',
-	authDomain: 'markdown-previewer-e2cdb.firebaseapp.com',
-	databaseURL: 'https://markdown-previewer-e2cdb.firebaseio.com',
-	projectId: 'markdown-previewer-e2cdb',
-	storageBucket: 'markdown-previewer-e2cdb.appspot.com',
-	messagingSenderId: '512506420964',
-	appId: '1:512506420964:web:ace436d8b85819499516f1'
+	apiKey: '***REMOVED***',
+	authDomain: '***REMOVED***',
+	databaseURL: '***REMOVED***',
+	projectId: 'ptrchoi-portfolio',
+	storageBucket: '***REMOVED***',
+	messagingSenderId: '***REMOVED***',
+	appId: '1:***REMOVED***:web:***REMOVED***',
+	measurementId: '***REMOVED***'
 };
 
 // Initialize Firebase
